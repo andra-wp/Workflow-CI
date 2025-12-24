031fd888fddad0c0b673e1e37472dac95cda731c
Andra Wikanjaya <andrawpt@gmail.com>
2025-12-24 13:01:42 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,29 +7,24 @@ import mlflow.sklearn
 import pandas as pd
 from pathlib import Path
 
-mlflow.set_experiment("diabetes_al_experiment")
-
-
 def train_with_mlflow(df, target_col, model):
     mlflow.sklearn.autolog()
 
-    with mlflow.start_run(run_name="rf_training"):
+    X = df.drop(columns=[target_col])
+    y = df[target_col].astype(int)
 
-        X = df.drop(columns=[target_col])
-        y = df[target_col].astype(int)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X,
-            y,
-            test_size=0.2,
-            random_state=42,
-            stratify=y
-        )
+    model.fit(X_train, y_train)
 
-        model.fit(X_train, y_train)
-
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
     return model, acc
 
